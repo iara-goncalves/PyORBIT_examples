e9099c6bea8f77c5b65861eb38626c40f659ca01
Iara Goncalves <s233175@dtu.dk>
2025-08-21 17:03:11 +0200

    First commit of PyORBIT examples

---
+++ quickstart/job_submissions.sh
@@ -0,0 +1,38 @@
+#!/bin/sh 
+### General options 
+### -- specify queue -- 
+#BSUB -q hpc
+### -- set the job Name -- 
+#BSUB -J example_iara
+### -- ask for number of cores (default: 1) -- 
+#BSUB -n 4
+### -- specify that the cores must be on the same host -- 
+#BSUB -R "span[hosts=1]"
+### -- specify that we need 4GB of memory per core/slot -- 
+#BSUB -R "rusage[mem=4GB]"
+### -- specify that we want the job to get killed if it exceeds 5 GB per core/slot -- 
+#BSUB -M 5GB
+### -- set walltime limit: hh:mm -- 
+#BSUB -W 3:00 
+### -- set the email address -- 
+# please uncomment the following line and put in your e-mail address,
+# if you want to receive e-mail notifications on a non-default address
+#BSUB -u icogo@dtu.dk
+### -- send notification at start -- 
+#BSUB -B 
+### -- send notification at completion -- 
+#BSUB -N 
+### -- Specify the output and error file. %J is the job-id -- 
+### -- -o and -e mean append, -oo and -eo mean overwrite -- 
+#BSUB -o Output.out 
+
+cd /work2/lbuc/iara/GitHub/PyORBIT_examples/quickstart
+
+source /work2/lbuc/iara/anaconda3/etc/profile.d/conda.sh
+conda activate pyorbit
+
+# here follow the commands you want to execute with input.in as the input file
+pyorbit_run emcee HD189733_example01_onedataset.yaml > configuration_file_emcee_run.log
+pyorbit_results emcee HD189733_example01_onedataset.yaml
+
+conda deactivate
